d123f88564b461605d949cd1689fd5d41e8534e6
Matthew Barnes <mbarnes@fedoraproject.org>
2022-05-26 16:13:08 -0400

    Update scripts to take command-line arguments
    Useful for comparing an older Raspberry Pi OS image to latest.
    Works the same as before if no command-line arguments are given.

--- scripts/just-install.sh
+++ scripts/just-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_FILE=$(realpath $(ls *.img | tail --lines 1))
-CHROOT_PATH=$(realpath rootfs)
+IMAGE_FILE=$(realpath ${1:-$(ls *.img | tail -n 1)})
+CHROOT_PATH=$(realpath ${2:-arcade_rootfs})
 
 sudo ./init-arcade.yml -e cli_image_file=$IMAGE_FILE -e cli_chroot_path=$CHROOT_PATH -e cli_skip_build=true
--- scripts/safe-remove.sh
+++ scripts/safe-remove.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CHROOT_PATH=$(realpath rootfs)
+CHROOT_PATH=$(realpath ${1:-arcade_rootfs})
 
 sudo ./safe-remove.yml -e cli_chroot_path=$CHROOT_PATH
